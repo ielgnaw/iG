16a0498ce09c4cf8275096719f881b06638df822
ielgnaw <wuji0223@gmail.com>
2015-03-08 12:18:33 +0800

    fast.js

--- examples/1/main.amd.js
+++ examples/1/main.amd.js
@@ -17,42 +17,45 @@ define(function (require) {
     var exports = {};
 
     exports.init = function () {
-        console.log(ig.env);
-        var designWidth = 414;
-        var designHeight = 736;
-        var viewWidth = document.documentElement.clientWidth;
-        var viewHeight = document.documentElement.clientHeight;
-        var scale = viewWidth / designWidth;
-        canvas.width = designWidth;
-        canvas.height = viewHeight / scale;
-        canvas.style.width = viewWidth + 'px';
-        canvas.style.height = viewHeight + 'px';
+        // var designWidth = 414;
+        // var designHeight = 736;
+        // var viewWidth = document.documentElement.clientWidth;
+        // var viewHeight = document.documentElement.clientHeight;
+        // var scale = viewWidth / designWidth;
+        // canvas.width = designWidth;
+        // canvas.height = viewHeight / scale;
+        // canvas.style.width = viewWidth + 'px';
+        // canvas.style.height = viewHeight + 'px';
 
-        // var devicePixelRatio = window.devicePixelRatio || 1;
 
-        // var backingStoreRatio = ctx.webkitBackingStorePixelRatio
-        //     || ctx.mozBackingStorePixelRatio
-        //     || ctx.msBackingStorePixelRatio
-        //     || ctx.oBackingStorePixelRatio
-        //     || ctx.backingStorePixelRatio
-        //     || 1;
+        canvas.width = document.documentElement.clientWidth;
+        canvas.height = document.documentElement.clientHeight;
+        var devicePixelRatio = window.devicePixelRatio || 1;
 
-        // var ratio = devicePixelRatio / backingStoreRatio;
-        // console.warn(ratio);
+        var backingStoreRatio = ctx.webkitBackingStorePixelRatio
+            || ctx.mozBackingStorePixelRatio
+            || ctx.msBackingStorePixelRatio
+            || ctx.oBackingStorePixelRatio
+            || ctx.backingStorePixelRatio
+            || 1;
 
-        // if (devicePixelRatio !== backingStoreRatio) {
-        //     var oldWidth = canvas.width;
-        //     var oldHeight = canvas.height;
+        var ratio = devicePixelRatio / backingStoreRatio;
+        console.warn(ratio);
 
-        //     canvas.width = oldWidth * ratio;
-        //     canvas.height = oldHeight * ratio;
+        if (devicePixelRatio !== backingStoreRatio) {
+            var oldWidth = canvas.width;
+            var oldHeight = canvas.height;
 
-        //     canvas.style.width = oldWidth + 'px';
-        //     canvas.style.height = oldHeight + 'px';
+            canvas.width = oldWidth * ratio;
+            canvas.height = oldHeight * ratio;
 
-        //     ctx.scale(ratio, ratio);
-        //     console.warn(ctx);
-        // }
+            canvas.style.width = oldWidth + 'px';
+            canvas.style.height = oldHeight + 'px';
+
+            ctx.scale(ratio, ratio);
+            console.warn(ctx);
+            console.warn(canvas.width);
+        }
 
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 
--- src/core.js
+++ src/core.js
@@ -111,8 +111,58 @@
         };
     };
 
-    ig.resetMatrix = function () {
-
+    /**
+     * func.apply(thisContext, args);
+     *
+     * @param {Function} func 待执行函数
+     * @param {Object} thisContext 上下文
+     * @param {Array} args 参数
+     */
+    ig.fastApply = function (func, thisContext, args) {
+        switch (args.length) {
+            case 0:
+                return func.call(
+                    thisContext
+                );
+            case 1:
+                return func.call(
+                    thisContext, args[0]
+                );
+            case 2:
+                return func.call(
+                    thisContext, args[0], args[1]
+                );
+            case 3:
+                return func.call(
+                    thisContext, args[0], args[1], args[2]
+                );
+            case 4:
+                return func.call(
+                    thisContext, args[0], args[1], args[2], args[3]
+                );
+            case 5:
+                return func.call(
+                    thisContext, args[0], args[1], args[2], args[3], args[4]
+                );
+            case 6:
+                return func.call(
+                    thisContext, args[0], args[1], args[2], args[3], args[4], args[5]
+                );
+            case 7:
+                return func.call(
+                    thisContext, args[0], args[1], args[2], args[3], args[4], args[5], args[6]
+                );
+            case 8:
+                return func.call(
+                    thisContext, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]
+                );
+            case 9:
+                return func.call(
+                    thisContext, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]
+                );
+            default:
+                return func.apply(thisContext, args);
+        }
     };
 
 })(root || this, ig || {});
