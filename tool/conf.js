42bcec522d9290f120b90401715848d26ae3dcbb
ielgnaw <wuji0223@gmail.com>
2015-04-15 00:38:49 +0800

    refactor backup

--- tool/conf.js
+++ tool/conf.js
@@ -2,32 +2,33 @@
 exports.modules = {
     main: {name: 'ig/ig'},
     parts: [
+        {name: 'ig/easing', refName: 'easing'}
         // refName 指要挂载到 window.ig 这个全局空间上的名字
         // 如果无 refName 配置，意味着该模板只是 ig 内部调用的，不会被挂载在 window 上
-        {name: 'ig/util', refName: 'util'},     // window.ig.util
-        {name: 'ig/Event', refName: 'Event'},   // window.ig.Event
-        {name: 'ig/env', refName: 'env'},  // window.ig.env
-        {name: 'ig/Game', refName: 'Game'}, // window.ig.Game
-        {name: 'ig/Stage'},
-        {name: 'ig/DisplayObject', refName: 'DisplayObject'}, // window.ig.FrameMonitor
-        {name: 'ig/SpriteSheet', refName: 'SpriteSheet'}, // window.ig.SpriteSheet
-        {name: 'ig/resourceLoader'},
-        {name: 'ig/Stage'},
+        // {name: 'ig/util', refName: 'util'},     // window.ig.util
+        // {name: 'ig/Event', refName: 'Event'},   // window.ig.Event
+        // {name: 'ig/env', refName: 'env'},  // window.ig.env
+        // {name: 'ig/Game', refName: 'Game'}, // window.ig.Game
+        // {name: 'ig/Stage'},
+        // {name: 'ig/DisplayObject', refName: 'DisplayObject'}, // window.ig.FrameMonitor
+        // {name: 'ig/SpriteSheet', refName: 'SpriteSheet'}, // window.ig.SpriteSheet
+        // {name: 'ig/resourceLoader'},
+        // {name: 'ig/Stage'},
 
-        {name: 'ig/Shape/Ball', refName: 'Ball', folder: 'Shape'}, // window.ig.Shape.Ball
+        // {name: 'ig/Shape/Ball', refName: 'Ball', folder: 'Shape'}, // window.ig.Shape.Ball
 
-        {name: 'ig/geom/Circle', refName: 'Circle', folder: 'geom'}, // window.ig.geom.Circle
-        {name: 'ig/geom/Vector', refName: 'Vector', folder: 'geom'}, // window.ig.geom.Vector
-        {name: 'ig/geom/Polygon', refName: 'Polygon', folder: 'geom'}, // window.ig.geom.Polygon
-        {name: 'ig/geom/Rect', refName: 'Rect', folder: 'geom'}, // window.ig.geom.Rect
-        {name: 'ig/collision'},
+        // {name: 'ig/geom/Circle', refName: 'Circle', folder: 'geom'}, // window.ig.geom.Circle
+        // {name: 'ig/geom/Vector', refName: 'Vector', folder: 'geom'}, // window.ig.geom.Vector
+        // {name: 'ig/geom/Polygon', refName: 'Polygon', folder: 'geom'}, // window.ig.geom.Polygon
+        // {name: 'ig/geom/Rect', refName: 'Rect', folder: 'geom'}, // window.ig.geom.Rect
+        // {name: 'ig/collision'},
 
-        {name: 'ig/geom/polygon', folder: 'geom'},
-        {name: 'ig/domEvt'},
-        {name: 'ig/Bitmap', refName: 'Bitmap'},
-        {name: 'ig/Text', refName: 'Text'},
-        {name: 'ig/easing', refName: 'easing'},
-        {name: 'ig/Animation', refName: 'Animation'}
+        // {name: 'ig/geom/polygon', folder: 'geom'},
+        // {name: 'ig/domEvt'},
+        // {name: 'ig/Bitmap', refName: 'Bitmap'},
+        // {name: 'ig/Text', refName: 'Text'},
+        // {name: 'ig/easing', refName: 'easing'},
+        // {name: 'ig/Animation', refName: 'Animation'}
     ]
 };
 
