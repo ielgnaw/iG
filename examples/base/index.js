c666e9369aef277b8c9c1bd2222574b81b3babbe
ielgnaw <wuji0223@gmail.com>
2015-06-02 00:00:59 +0800

    up

--- examples/base/index.js
+++ examples/base/index.js
@@ -3,7 +3,7 @@
 /* global ig */
 
 window.onload = function () {
-    var gameFps = 100;
+    var gameFps = 60;
 
     function Circle(opts) {
         this.x = opts.x;
@@ -16,6 +16,7 @@ window.onload = function () {
     }
 
     Circle.prototype.draw = function () {
+        // console.warn(this.x);
         this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
         this.ctx.beginPath();
         this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
@@ -23,8 +24,13 @@ window.onload = function () {
     };
 
     Circle.prototype.update = function (dt) {
-        this.x += this.vx * dt * this.fps / 1000;
-        this.y += this.vy * dt * this.fps / 1000;
+        // this.x += this.vx * dt * this.fps / 1000;
+        // this.y += this.vy * dt * this.fps / 1000;
+
+        this.vx += this.ax * dt;
+        this.vy += this.ay * dt;
+        this.x += this.vx * dt; //  * (1 * 60)/ 1000;
+        this.y += this.vy * dt; //  * 1 / 100;
 
         if (this.x - this.radius <= 0 || this.x + this.radius >= this.ctx.canvas.width) {
             this.vx = -this.vx;
@@ -34,11 +40,17 @@ window.onload = function () {
         }
     }
 
+    var canvas = document.querySelector('#time-based-canvas');
+    var canvasWidth = canvas.width;
+    var canvasHeight = canvas.height;
+
     var circle = new Circle({
         x: 15,
         y: 30,
         vx: 10,
         vy: 0,
+        ax: 1,
+        ay: 0,
         radius: 15,
         ctx: document.querySelector('#time-based-canvas').getContext('2d'),
         fps: gameFps
