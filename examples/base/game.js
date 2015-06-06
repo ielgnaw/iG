c82c10cdbfd309953ebd5fc634b4c3086b53e841
ielgnaw <wuji0223@gmail.com>
2015-06-06 16:03:11 +0800

    up

--- examples/base/game.js
+++ examples/base/game.js
@@ -26,19 +26,19 @@ window.onload = function () {
             = (e.data.loadedCount / e.data.total).toFixed(1) * 100 + '%'
     }).on('loadResDone', function (e) {
         // console.warn(e, 'loadResDone');
-        simulateEvent();
-        this.asset.a2o.play();
+        // simulateEvent();
+        // this.asset.a2o.play();
     });
 
-    function simulateEvent() {
-        var e = document.createEvent('HTMLEvents');
-        e.initEvent('click',true, true);
-        document.dispatchEvent(e);
-    }
+    // function simulateEvent() {
+    //     var e = document.createEvent('HTMLEvents');
+    //     e.initEvent('click',true, true);
+    //     document.dispatchEvent(e);
+    // }
 
-    document.onclick = function () {
-        alert(3)
-    }
+    // document.onclick = function () {
+    //     alert(3)
+    // }
 
     game1.start();
 
@@ -60,99 +60,4 @@ window.onload = function () {
     // }, function () {
     //     console.warn(arguments, 111);
     // });
-
-    // for (var i = 0; i < 10; i++) {
-    //     (function (index) {
-    //         setTimeout(function () {
-    //             console.warn(index);
-    //         }, index * 300)
-    //     })(i)
-    // }
-
-
-    // setInterval(function () {
-    //     ig.util.debounce(function () {
-    //         console.warn(1);
-    //     }, 1000)();
-    //     ig.util.throttle(function () {
-    //         console.warn(2);
-    //     }, 200)();
-    // }, 30);
-
-
-
-
-    // console.warn(ig);
-    // var fpsNode = document.querySelector('#fps');
-
-    // var game = new ig.Game(
-    //     {
-    //         canvas: canvas,
-    //         name: 'fps-test-game',
-    //         fps: 50,
-    //         maximize: 1,
-    //         // horizontalPageScroll: 100
-    //     }
-    // ).on('beforeGameRender', function (d) {
-    //     // console.warn('beforeGameRender', d);
-    // }).on('afterGameRender', function (d) {
-    //     // console.warn('afterGameRender', d);
-    // }).on('gameFPS', function (d) {
-    //     fpsNode.innerHTML = 'fps: ' + d.data.fps;
-    // })/*.start(function () {
-    //     console.warn('start callback');
-    // })*/;
-
-    /*document.querySelector('.control').onclick = function (e) {
-        e.stopPropagation();
-        e.preventDefault();
-        var target = e.target || e.srcElement;
-
-        if (target.tagName.toLowerCase() === 'a') {
-            var type = target.getAttribute('data');
-
-            switch (type) {
-                case 'start':
-                    game.start(function () {
-                        game.loadResource(
-                            [
-                                '/examples/img/1.jpg',
-                                {
-                                    id: 'bg',
-                                    src: '/examples/img/bg.jpg'
-                                },
-                                {
-                                    id: 'jsonData',
-                                    src: '/examples/resource-loader/data/test.json'
-                                },
-                                {
-                                    id: 'textData',
-                                    src: '/examples/resource-loader/data/text.text'
-                                }
-                            ],
-                            function (d) {
-                                console.log('all done', d);
-                            },
-                            {
-                                processCallback: function (d) {
-                                    console.warn('process', d);
-                                }
-                            }
-                        );
-                    });
-                    break;
-                case 'stop':
-                    game.stop();
-                    break;
-                case 'pause':
-                    game.pause();
-                    break;
-                case 'resume':
-                    game.resume();
-                    break;
-                default:
-            }
-        }
-    };*/
-
 };
