07a414a2e3d44097407f7093571663d3f60b53f0
ielgnaw <wuji0223@gmail.com>
2015-06-30 17:51:19 +0800

    parent: spritesheet, sub: Text. done

--- examples/base/children/5.js
+++ examples/base/children/5.js
@@ -5,6 +5,7 @@
 
 window.onload = function () {
     var canvas = document.querySelector('#canvas');
+    var ctx = canvas.getContext('2d');
 
     var game1 = new ig.Game({
         canvas: canvas,
@@ -15,26 +16,41 @@ window.onload = function () {
             // {id: 'bg1', src: '/examples/img/base/bg1.png'},
             // {id: 'bg2', src: '/examples/img/base/bg2.png'},
             // {id: 'bg3', src: '/examples/img/base/bg3.png'},
-            {id: 'playBut', src: '/examples/img/base/playBut.png'},
+            {id: 'spriteSheetImg', src: '/examples/img/base/sprite-sheet1.png'},
+            {id: 'spriteSheetImg2', src: '/examples/img/base/sprite-sheet2.jpg'},
             // {id: 'test2', src: '/examples/img/base/2.png'},
-            {id: 'runnerBox', src: '/examples/img/base/runner-box.png'},
-            // {id: 'testData', src: './data/test.json'},
+            {id: 'spriteSheetData', src: './data/sprite-sheet1.json'},
+            {id: 'spriteSheetData2', src: './data/sprite-sheet2.json'},
             // {id: 'text', src: './data/text.text'},
             // {id: 'a2o', src: ['./data/a2.mp3','./data/a2.ogg'], opts: {loop: true}},
             // '/examples/img/base/boom.png'
         ]
     }).on('loadResProcess', function (e) {
         document.querySelector('#load-process').innerHTML
-            = 'loadProcess: ' + (e.data.loadedCount / e.data.total).toFixed(1) * 100 + '%';
+            = 'loadProcess: ' + (e.data.loadedCount / e.data.total).toFixed(1) * 100 + '%'
     }).on('loadResDone', function (e) {
     });
 
     var stage1 = game1.createStage({
         name: 'stage-name1',
+        // parallaxOpts: [
+        //     {
+        //         image: 'bg',
+        //         anims: [
+        //             {
+        //                 ax: 1,
+        //                 ay: 1
+        //             },
+        //             {
+        //                 ax: -1,
+        //                 ay: 1
+        //             }
+        //         ],
+        //         animInterval: 100
+        //     }
+        // ],
         captureFunc: function (e) {
-            console.warn(e);
-            console.warn(bitmap1.hitTestPoint(e.x, e.y));
-            // console.warn(text1.hitTestPoint(e.x, e.y));
+            console.warn(spritesheet1.hitTestPoint(e.x, e.y));
         }
     });
 
@@ -49,47 +65,46 @@ window.onload = function () {
         isBold: true,
         angle: 0,
         debug: 1,
-        // zIndex: 100,
-        fillStyle: '#fff',
-        mouseEnable: true,
+        zIndex: 100,
+        fillStyle: '#f00',
+        // mouseEnable: true,
         moveFunc: function (d) {
-            d.domEvent.preventDefault();
             // console.warn(d);
             this.move(d.x, d.y);
         },
-        // useCache: false
-    });
+    })
 
-    var bitmap1 = stage1.addDisplayObject(
-        new ig.Bitmap({
-            name: 'bitmap1',
-            // image: '/examples/img/base/2.png',
-            image: 'runnerBox',
-            // sWidth: 110,
-            // sHeight: 110,
-            debug: 1,
+    var spritesheet1 = stage1.addDisplayObject(
+        new ig.SpriteSheet({
+            name: 'spritesheet1',
+            // image: '/examples/img/base/sprite-sheet1.png',
+            // image: 'spriteSheetImg',
+            // sheet: 'spriteSheetData',
+            // sheet: './data/sprite-sheet1.json',
+            // sheetKey: 'red',
+
+            image: 'spriteSheetImg2',
+            sheet: './data/sprite-sheet2.json',
+            jumpFrames: 5,
             x: 150,
             y: 150,
-            width: 80,
-            height: 80,
-            mouseEnable: true,
+            debug: 1,
+            // vx: 1,
+            // vy: 2,
+            // width: 100,
+            // height: 200,
             children: [text1],
-            vx: 1,
+            mouseEnable: true,
             moveFunc: function (d) {
                 d.domEvent.preventDefault();
-                // console.warn(d);
                 this.move(d.x, d.y);
             },
+            // useCache: false
         })
     );
-
-    bitmap1.step = function (dt, stepCount) {
-        this.angle += 1;
-        // this.alpha -= 0.01;
-        // this.setScale(this.scaleX + 0.01, this.scaleY + 0.01);
-        // this.scaleX += 0.01;
-        // this.scaleY += 0.01;
-        // console.warn(dt);
+    spritesheet1.step = function (dt, stepCount, requestID) {
+        // console.warn(arguments);
+        // this.angle += 1;
         this.vx += this.ax * dt;
         this.vx *= this.frictionX * dt;
         this.vy += this.ay * dt;
@@ -115,31 +130,18 @@ window.onload = function () {
     };
 
     // setTimeout(function () {
-    //     rectangle1.move(200, 250);
+    //     // spritesheet1.change();
+    //     spritesheet1.change(
+    //         // ig.util.extend(true, {}, game1.asset.spriteSheetData.redCapture, {x: 20, y: 30})
+    //         ig.util.extend(true, {
+    //             asset: game1.asset.spriteSheetImg,
+    //             isOnce: true,
+    //             onceDone: function () {
+    //                 // console.warn(arguments, 'onceDone');
+    //             }
+    //         }, game1.asset.spriteSheetData.red)
+    //     );
     // }, 3000);
-
-    // var text1 = stage1.addDisplayObject(
-    //     new ig.Text({
-    //         name: 'text1',
-    //         content: 'oh no....',
-    //         x: 200,
-    //         y: 400,
-    //         // scaleX: 0.5,
-    //         // scaleY: 0.5,
-    //         size: 20,
-    //         isBold: true,
-    //         angle: 0,
-    //         debug: 1,
-    //         zIndex: 100,
-    //         fillStyle: '#f00',
-    //         mouseEnable: true,
-    //         moveFunc: function (d) {
-    //             // console.warn(d);
-    //             this.move(d.x, d.y);
-    //         },
-    //         // useCache: false
-    //     })
-    // );
 
     // text1.step = function (dt, stepCount) {
     //     this.angle += 1;
